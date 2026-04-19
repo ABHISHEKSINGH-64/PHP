b214d93e0a948f34d25c4c48ee7ca0f89208bc99
Abhishek Singh <abhisherajput64@gmail.com>
2026-04-19 22:06:46 +0530

    update

--- API/logincandidates.php
+++ API/logincandidates.php
@@ -1,44 +1,43 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "Leapstart";
+    $username = "postgres";
+    $password = "Abhishek@123";
 
-    $host = 'localhost';
-    $port = '5432';
-    $dbname = 'Leapstart';
-    $username = 'postgres';
-    $password = 'Abhishek@123';
+    $idno = trim($_POST["idno"]);
+    $user_password = trim($_POST["user_password"]);
 
-    $idno = $_POST["idno"];
-    $user_password = $_POST["user_password"];
-
-    try {
-
+    try{
         $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
         $pdo = new PDO($dsn, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $pdo->prepare ("SELECT COUNT(*) as count FROM candidates WHERE idno = :idno AND password = :user_password ");
+        $stmt = $pdo->prepare('SELECT COUNT(*) as count 
+                               FROM candidates 
+                               WHERE idno = :idno 
+                               AND "password" = :user_password');
 
         $stmt->execute([
-            'idno' => $idno,
-            'user_password' => $user_password
+            ':idno' => $idno,
+            ':user_password' => $user_password
         ]);
 
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if($result['count'] == 1){
-            echo "Connected Successfully";
-        } else {
+        if($result["count"] > 0){
+            echo "Login Success";
+        }else{
             echo "Invalid ID Number or Password";
         }
 
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+    }catch(PDOException $e){
+        echo "Database Error: " . $e->getMessage();
     }
 
-}
-else
-{
+}else{
     echo "Use POST Method";
 }
 ?>
